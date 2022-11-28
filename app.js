99ef7f6c020a8ada8ef4f0a7f829b127f59a57e5
Aman <Sharmaamqn@gmail.com>
2022-11-28 16:24:48 +0530

    write the logic for the game to work

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
 let maximum = parseInt(prompt("Enter the maximum number:"));
-let guess = Math.floor(Math.random*maximum)
+while (!maximum) {
+  maximum = parseInt(prompt("Enter the maximum number!"));
+}
+
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+
+let guess = prompt("Enter your first guess!");
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+  if (guess === "q") break;
+  attempts++;
+  if (guess > targetNum) {
+    guess = prompt("Too high! Enter a new guess:");
+  } else if (guess < targetNum) {
+    guess = prompt("Too low! Enter a new guess:");
+  } else {
+    guess = prompt(
+      `Your guess is ${guess}, which is not a number, please guess a number.`
+    );
+  }
+}
+
+if (guess === "q") {
+  alert("Okay, you quit!");
+} else {
+  alert(`Congratulations! You got it in ${attempts} guesses.`);
+}
